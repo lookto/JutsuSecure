47add5834a486dba68e64f54619b3d1db14f1ba3
lt <lt@bitsandlikes.de>
2024-01-20 11:39:06 +0100

    init frontend

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} **/
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang='ts'>
+
+// INTERFACES
+
+// PROPS, STORES, EMITS, ...
+
+// REACTIVE VARS
+
+// VARS
+
+// FUNCTIONS
+
+// LIFECYCLE HOOKS, WATCHERS
+
+</script>
+<template>
+  <div>
+    Hallo Welt
+  </div>
+</template>
+<style scoped>
+</style>
+<style>
+</style>
